7e1cf91bbd61d25610cae8e41fe021e14bd45441
Jean-Louis Queguiner <jean-louis.queguiner@gadz.org>
2017-07-18 17:49:59 +0200

    Add files via upload

---
+++ app/config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'test2',
+		'encoding' => 'UTF8'
+	);
+}
+?>
